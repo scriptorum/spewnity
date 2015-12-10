86f371c338411329080bf8d7d646532658fcf1f1
Eric Lund <github@egotron.com>
2015-12-10 14:10:05 -0500

    You're a mean one, Mr. Grinch.

--- Editor/ExportWithLayers.cs
+++ Editor/ExportWithLayers.cs
@@ -6,15 +6,15 @@ namespace Spewnity
 {
 	public static class ExportWithLayers
 	{
-		[MenuItem("Custom/Export package with layers")]
+		[MenuItem("Custom/Export Package Plus")]
 		public static void ExportPackage()
 		{
 			string[] projectContent = new string[] 
-		{
-			"Assets",  // exports your prefabs
-			"ProjectSettings/TagManager.asset", // exports tags, layers, and sorting layers
-			"ProjectSettings/Physics2DSettings.asset" // exports 2D layer collision matrix
-		};
+			{
+				"Assets",  // exports your prefabs
+				"ProjectSettings/TagManager.asset", // exports tags, layers, and sorting layers
+				"ProjectSettings/Physics2DSettings.asset" // exports 2D layer collision matrix
+			};
 			string outName = PlayerSettings.productName + ".unitypackage";
 			AssetDatabase.ExportPackage(projectContent, outName,
             ExportPackageOptions.Interactive | ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies);

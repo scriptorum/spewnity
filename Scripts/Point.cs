71c8820bd8d7cf25ce97c8666e3665704e831e41
Eric Lund <github@egotron.com>
2016-09-19 18:03:04 -0400

    Now Serializable. Added comparators and Add function.

--- Scripts/Point.cs
+++ Scripts/Point.cs
@@ -3,20 +3,47 @@
 //
 namespace Spewnity
 {
+	[System.Serializable]
 	public struct Point
 	{
 		public int x;
 		public int y;
-		
+
 		public Point(int x, int y)
 		{
 			this.x = x;
 			this.y = y;
 		}
-		
+
 		override public string ToString()
 		{
 			return "Point(" + this.x + "," + this.y + ")";
+		}
+
+		public void Add(Point otherPoint)
+		{
+			this.x += otherPoint.x;
+			this.y += otherPoint.y;
+		}
+
+		public override bool Equals(System.Object obj)
+		{
+			return obj is Point && this == (Point) obj;
+		}
+
+		public override int GetHashCode()
+		{
+			return x ^ y;
+		}
+
+		public static bool operator ==(Point left, Point right)
+		{
+			return (left.x == right.x && left.y == right.y);
+		}
+
+		public static bool operator !=(Point x, Point y)
+		{
+			return !(x == y);
 		}
 	}
 }

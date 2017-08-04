0fcd77b978110920ffd8f6a0f6fef88b31e4036f
Eric Lund <github@egotron.com>
2017-08-04 09:38:59 -0400

    Sort order is value is now customizable in VerticalDrawOrder

--- Scripts/VerticalDrawOrder.cs
+++ Scripts/VerticalDrawOrder.cs
@@ -1,32 +1,29 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Spewnity
 {
-	// When assigned to a set of game objects, ensures that
-	// the lower objects are positioned in front of higher objects.
-	// For simulating depth in a 2D game.
-	//
-	// This can be done either by manipulating the Z position,
-	// or the SpriteRenderer sort order.
-	public class VerticalDrawOrder : MonoBehaviour
-	{
-		private SpriteRenderer sr;
-		//	private float amount = 0.01f;
+    // When assigned to a set of game objects, ensures that
+    // the lower objects are positioned in front of higher objects.
+    // For simulating depth in a 2D game.
+    //
+    // This can be done either by manipulating the Z position,
+    // or the SpriteRenderer sort order.
+    public class VerticalDrawOrder : MonoBehaviour
+    {
+        public int baseOrder = 0;
+        public int orderMultiplier = -1000;
 
-		void Awake()
-		{
-			sr = GetComponent<SpriteRenderer>();
-		}
+        private SpriteRenderer sr;
 
-		void Update()
-		{
-			//		if(transform.hasChanged)
-			//		{
-			sr.sortingOrder = (int) (transform.position.y * -1000);
-			//			transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y * amount);
-			//			transform.hasChanged = false; // this is competing with FlipAndFacePlayer
-			//		}
-		}
-	}
+        void Awake()
+        {
+            sr = GetComponent<SpriteRenderer>();
+        }
+
+        void Update()
+        {
+            sr.sortingOrder = (int)(transform.position.y * orderMultiplier) + baseOrder;
+        }
+    }
 }

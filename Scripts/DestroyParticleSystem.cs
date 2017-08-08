78c8d0f0c4a411423c40cca56d59d63b619328d2
Eric Lund <github@egotron.com>
2017-08-08 11:03:38 -0400

    Added DestroyParticleSystem and DontDestroyOnLoad

---
+++ Scripts/DestroyParticleSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spewnity
+{
+	/**
+		Attach this component to a GameObject with a particle system. When the particle system is 
+		no longer producing particles and all particles are dead, it will destroy the GameObject.
+	 */
+    public class DestroyParticleSystem : MonoBehaviour
+    {
+		[Tooltip("Includes child particle systems in the check")]
+		public bool withChildren = true;
+
+        private ParticleSystem ps;
+
+        void Awake()
+        {
+            ps = gameObject.GetComponent<ParticleSystem>();
+			if(ps == null)
+				throw new UnityException("DestroyParticleSystem must be attached a GameObject with a ParticleSystem.");
+        }
+
+        void Update()
+        {
+            if (!ps.IsAlive(withChildren))
+                Destroy(gameObject);
+        }
+    }
+}
---
+++ Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spewnity
+{
+	/**
+		When attached to a GameObject, it will be marked as DontDestroyOnLoad. This means it will survive
+		transitions when loading new scenes non-additively.
+	 */
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}

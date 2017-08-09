ea2712a997be9517904a1959c8cb487343b41819
Eric Lund <github@egotron.com>
2017-08-09 10:05:04 -0400

    Updated toolkit comments with doc formatting

--- Scripts/Toolkit.cs
+++ Scripts/Toolkit.cs
@@ -1,21 +1,26 @@
-﻿//TODO Add StartCoroutine versions of the Lerps? I'll have to extend them off MonoBehaviour then.
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+
+// TODO Add StartCoroutine versions of the Lerps? I'll have to extend them off MonoBehaviour then.
+// TODO Should tween functions should check if object is null and end coroutine if the case? Does that lead to bug masking?
+// TODO LerpColor should support null endColor if AnimationCurve is specified
+// TODO Rewrite Join using LINQ, dammit
 
 namespace Spewnity
 {
     public static class Toolkit
     {
-        /**
-         * Tweens the transform from its current position to endPos in world space.
-         * Remember to wrap in a StarCoroutine().
-         * Action triggered at end of tween. Example Action: (t) => Debug.Log("Transform complete!")
-         * If endPos is null, curve is required and treated as a current position multipler. Z is not affected.
-         * Otherwise tweens between current and endPos, optionally using 0-1 AnimationCurve to adjust easing.
-         * TODO Should tween functions should check if object is null and end coroutine if the case? Does that lead to bug masking?
-         */
+        /// <summary>
+        /// Tweens the transform from its current position to endPos in world space.
+        /// </summary>
+        /// <param name="tform">The transform to tween</param>
+        /// <param name="endPos">If endPos is null, curve is required and treated as a current position multipler. Z is not affected. Otherwise tweens between current and endPos, optionally using 0-1 AnimationCurve to adjust easing.</param>
+        /// <param name="duration">Duration of lerp in seconds</param>
+        /// <param name="curve">Optional easing curve</param>
+        /// <param name="onComplete">Action triggered at end of tween. Example Action: (t) => Debug.Log("Transform complete!")</param>
+        /// <returns>Coroutine suitable for passing to StartCoroutine()</returns>
         public static IEnumerator LerpPosition(this Transform tform, Vector3? endPos, float duration,
             AnimationCurve curve = null, System.Action<Transform> onComplete = null)
         {
@@ -38,14 +43,15 @@ namespace Spewnity
             if (onComplete != null) onComplete.Invoke(tform);
         }
 
-        /**
-         * Tweens the transform from its current scale to endScale in local space.
-         * Remember to wrap in a StarCoroutine().
-         * Action triggered at end of tween. Example Action: (t) => Debug.Log("Transform complete!")
-         * If endScale is null, curve is required and treated as a current scale multipler. Z is not affected.
-         * Otherwise tweens between current and endScale, optionally using 0-1 AnimationCurve to adjust easing.
-         * TODO Should tween functions should check if object is null and end coroutine if the case? Does that lead to bug masking?
-         **/
+        /// <summary>
+        /// Tweens the transform from its current scale to endScale in local space.
+        /// </summary>
+        /// <param name="tform">The transform to tween</param>
+        /// <param name="endScale">If endScale is null, curve is required and treated as a current scale multipler. Z is not affected. Otherwise tweens between current and endScale, optionally using 0-1 AnimationCurve to adjust easing.</param>
+        /// <param name="duration">Duration of lerp in seconds</param>
+        /// <param name="curve">Optional easing curve</param>
+        /// <param name="onComplete">Action triggered at end of tween. Example Action: (t) => Debug.Log("Transform complete!")</param>
+        /// <returns>Coroutine suitable for passing to StartCoroutine()</returns>
         public static IEnumerator LerpScale(this Transform tform, Vector3? endScale, float duration,
             AnimationCurve curve = null, System.Action<Transform> onComplete = null)
         {
@@ -68,9 +74,16 @@ namespace Spewnity
             if (onComplete != null) onComplete.Invoke(tform);
         }
 
-        // Tweens the between two colors. Sends the interpolated color to onUpdate().
-        // Remember to wrap in a StarCoroutine().
-        // TODO Support null endColor if AnimationCurve is specified
+        /// <summary>
+        /// Tweens the between two colors. Sends the interpolated color to onUpdate().
+        /// </summary>
+        /// <param name="startColor">The initial color to tween from</param>
+        /// <param name="endColor">The final color to tween to</param>
+        /// <param name="duration">Duration of lerp in seconds</param>
+        /// <param name="onUpdate">Callback that receives the tweened color. LerpColor cannot tween-in-place, so use this function to assign the color as needed.</param>
+        /// <param name="curve">Optional easing curve</param>
+        /// <param name="onComplete">Action triggered at end of tween. Example Action: (t) => Debug.Log("Transform complete!")</param>
+        /// <returns>Coroutine suitable for passing to StartCoroutine()</returns>
         public static IEnumerator LerpColor(this Color startColor, Color endColor, float duration, System.Action<Color> onUpdate,
             AnimationCurve curve = null, System.Action onComplete = null)
         {
@@ -85,9 +98,16 @@ namespace Spewnity
             if (onComplete != null) onComplete.Invoke();
         }
 
-        // Tweens the between two float values. Sends the interpolated float to onUpdate().
-        // Remember to wrap in a StarCoroutine().
-        // TODO Support null endColor if AnimationCurve is specified
+        /// <summary>
+        /// Tweens the between two float values. Sends the interpolated float to onUpdate().        
+        /// </summary>
+        /// <param name="startValue">The initial value to tween from</param>
+        /// <param name="endValue">The final value to float to</param>
+        /// <param name="duration">Duration of lerp in seconds</param>
+        /// <param name="onUpdate">Callback that receives the tweened float. LerpFloat cannot tween-in-place, so use this function to assign the float as needed.</param>
+        /// <param name="curve">Optional easing curve</param>
+        /// <param name="onComplete">Action triggered at end of tween. Example Action: (t) => Debug.Log("Transform complete!")</param>
+        /// <returns>Coroutine suitable for passing to StartCoroutine()</returns>
         public static IEnumerator LerpFloat(this float startValue, float endValue, float duration, System.Action<float> onUpdate,
             AnimationCurve curve = null, System.Action onComplete = null)
         {
@@ -102,7 +122,12 @@ namespace Spewnity
             if (onComplete != null) onComplete.Invoke();
         }
 
-        // Snaps the XY component of a Vector3 to a 45 degree angle
+        /// <summary>
+        /// Snaps the XY component of a Vector3 to a 45 degree angle
+        /// </summary>
+        /// <param name="v3">Vector3</param>
+        /// <param name="snapAngle">The angle to snap, in degrees</param>
+        /// <returns>The angle, rounded to some multiple of 45, in degrees</returns>
         public static Vector3 Snap45(this Vector3 v3, float snapAngle)
         {
             float angle = Vector3.Angle(v3, Vector3.up);
@@ -118,7 +143,9 @@ namespace Spewnity
             return q * v3;
         }
 
-        // Shuffles an array or list in place. Also returns the input.
+        /// <summary>
+        /// Shuffles an array or list in place. Also returns the input.
+        /// </summary>
         public static IList<T> Shuffle<T>(this IList<T> ilist)
         {
             for (int i = ilist.Count - 1; i > 0; i--)
@@ -134,46 +161,63 @@ namespace Spewnity
             return ilist;
         }
 
-        // Integer-based absolute value
+        /// <summary>
+        /// Integer-based absolute value
+        /// </summary>
         public static int Abs(this int val)
         {
             return val < 0 ? -val : val;
         }
 
-        // Integer based sign
+        /// <summary>
+        /// Integer based sign
+        /// </summary>
         public static int Sign(this int val)
         {
             return val < 0 ? -1 : (val == 0 ? 0 : 1);
         }
 
-        // Returns the absolute path to the Transform
+        /// <summary>
+        /// Returns the absolute path to the Transform
+        /// </summary>
         public static string GetFullPath(this Transform o)
         {
             if (o.parent == null) return "/" + o.name;
             return o.parent.GetFullPath() + "/" + o.name;
         }
 
-        // Returns the absolute path to the GameObject
+        /// <summary>
+        /// Returns the absolute path to the GameObject
+        /// </summary>
         public static string GetFullPath(this GameObject o)
         {
             return GetFullPath(o.transform);
         }
 
-        // Returns one random object from the list or array
-        // Throws an assert if the collection is empty
+        // 
+        /// <summary>
+        /// Returns one random object from the list or array 
+        /// </summary>
+        /// <para>
+        /// Throws an assert if the collection is empty
+        /// </para>
         public static T Rnd<T>(this IList<T> collection)
         {
             Debug.Assert(collection.Count > 0);
             return collection.ElementAt(Random.Range(0, collection.Count));
         }
 
-        // Returns true or false randomly
+        /// <summary>
+        /// Returns true or false randomly 
+        /// </summary>
         public static bool CoinFlip()
         {
             return (Random.value >= 0.5f);
         }
 
-        // Swaps two values, just awful, I hate myself for writing it
+        /// <summary>
+        /// Swaps two values, just awful, I hate myself for writing it 
+        /// </summary>
         public static void Swap<T>(ref T a, ref T b)
         {
             T temp = a;
@@ -181,8 +225,9 @@ namespace Spewnity
             b = temp;
         }
 
-        // Joins an array or list of some type into a comma separated string
-        // TODO Learn some damn LINQ will you
+        /// <summary>
+        /// Joins an array or list of some type into a comma separated string
+        /// </summary>
         public static string Join<T>(this IList<T> iList, string delim = ",")
         {
             string ret = "";
@@ -195,13 +240,18 @@ namespace Spewnity
             return ret;
         }
 
-        // True if the string is null or has no content
+        /// <summary>
+        /// True if the string is null or has no content
+        /// </summary>
         public static bool IsEmpty(this string str)
         {
             return (str == null || str == "");
         }
 
-        // Returns the named child object of the transform, throws an assert if not found
+        /// <summary>
+        /// Returns the named child object of the Transform.
+        /// <para>Throws an assert if not found</para>
+        /// </summary>
         public static Transform GetChild(this Transform tform, string name)
         {
             Transform child = tform.Find(name);
@@ -209,13 +259,18 @@ namespace Spewnity
             return child;
         }
 
-        // Returns the named child object of the gameobject, throws an assert if not found
+        /// <summary>
+        /// Returns the named child object of the GameObject.
+        /// <para>Throws an assert if not found</para>
+        /// </summary>
         public static GameObject GetChild(this GameObject go, string name)
         {
             return go.transform.GetChild(name).gameObject;
         }
 
-        // Returns all game objects in the scene
+        /// <summary>
+        /// Returns all game objects in the scene 
+        /// </summary>
         public static List<GameObject> GetAllObjects()
         {
             List<GameObject> list = new List<GameObject>();
@@ -230,9 +285,11 @@ namespace Spewnity
             return list;
         }
 
-        // Returns the typed component of the object pointed to by the path.
-        // Throws an assert if the object or component cannot be found.
-        // E.g., Camera cam = Toolkit.GetComponentOf<Camera>("/Camera");
+        /// <summary>
+        /// Returns the typed component of the object pointed to by the path. 
+        /// <para>Throws an assert if the object or component cannot be found.</para>
+        /// </summary>
+        /// <example>E.g., Camera cam = Toolkit.GetComponentOf&lt;Camera&gt;("/Camera");</example>
         public static T GetComponentOf<T>(string path)
         {
             GameObject go = GameObject.Find(path);
@@ -242,8 +299,10 @@ namespace Spewnity
             return component;
         }
 
-        // Destroys all children underneath this transform, but does
-		// not destroy the transform's GameObject itself.
+        /// <summary>
+        /// Destroys all children underneath this Transform, but does
+        /// not destroy the Transform's GameObject itself.
+        /// </summary>
         public static void DestroyChildren(this Transform tform)
         {
             foreach(Transform child in tform)
@@ -252,22 +311,30 @@ namespace Spewnity
             }
         }
 
-        // Destroys all children underneath this GameObject, but does
-		// not destroy this GameObject itself.
+        /// <summary>
+        /// Destroys all children underneath this GameObject, but does
+        /// not destroy the Transform's GameObject itself.
+        /// </summary>
         public static void DestroyChildren(this GameObject go)
-		{
-			go.transform.DestroyChildren();
-		}
+        {
+            go.transform.DestroyChildren();
+        }
 
-		// Same as DestroyChildren, but does so immediately
+        // 
+        /// <summary>
+        /// Same as DestroyChildren, but does so immediately 
+        /// <see cref="DestroyChildren"/>
+        /// </summary>
         public static void DestroyChildrenImmediately(this Transform tform)
         {
             for (int i = tform.childCount - 1; i >= 0; i--)
                 GameObject.DestroyImmediate(tform.GetChild(i).gameObject);
         }
 
-		// Throws a NRE if this object is null, with an optional message
-		// Usage: Camera c = GetComponent<Camera>(); c.ThrowIfNull();
+        /// <summary>
+        /// Throws a NullReferenceException if this object is null, with an optional message 
+        /// <example>Camera c = GetComponent&lt;Camera&gt;(); c.ThrowIfNull();</example>
+        /// </summary>
         public static void ThrowIfNull(this object o, string msg = "NullReferenceException")
         {
             if (o == null) throw new System.NullReferenceException(msg);

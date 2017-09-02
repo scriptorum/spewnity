85678914a5d070143a320609b3533f2638faca6a
Eric Lund <github@egotron.com>
2017-09-02 09:54:25 -0400

    Point improvements: + operator and Vector3 conversion

--- Assets/Scripts/Spewnity/Point.cs
+++ Assets/Scripts/Spewnity/Point.cs
@@ -5,47 +5,62 @@
 //
 namespace Spewnity
 {
-	[System.Serializable]
-	public struct Point
-	{
-		public int x;
-		public int y;
+    [System.Serializable]
+    public struct Point
+    {
+        public int x;
+        public int y;
 
-		public Point(int x, int y)
-		{
-			this.x = x;
-			this.y = y;
-		}
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
 
-		override public string ToString()
-		{
-			return "Point(" + this.x + "," + this.y + ")";
-		}
+        override public string ToString()
+        {
+            return "Point(" + this.x + "," + this.y + ")";
+        }
 
-		public void Add(Point otherPoint)
-		{
-			this.x += otherPoint.x;
-			this.y += otherPoint.y;
-		}
+        public void Add(Point otherPoint)
+        {
+            this.x += otherPoint.x;
+            this.y += otherPoint.y;
+        }
 
-		public override bool Equals(System.Object obj)
-		{
-			return obj is Point && this == (Point) obj;
-		}
+        public override bool Equals(System.Object obj)
+        {
+            return obj is Point && this == (Point) obj;
+        }
 
-		public override int GetHashCode()
-		{
-			return x ^ y;
-		}
+        public override int GetHashCode()
+        {
+            return x ^ y;
+        }
 
-		public static bool operator ==(Point left, Point right)
-		{
-			return (left.x == right.x && left.y == right.y);
-		}
+        public static bool operator == (Point left, Point right)
+        {
+            return (left.x == right.x && left.y == right.y);
+        }
 
-		public static bool operator !=(Point x, Point y)
-		{
-			return !(x == y);
-		}
-	}
+        public static bool operator != (Point x, Point y)
+        {
+            return !(x == y);
+        }
+
+        public static Point operator + (Point left, Point right)
+        {
+            return new Point(left.x + right.x, left.y + right.y);
+        }
+
+        public static implicit operator UnityEngine.Vector2(Point p)
+        {
+            return new UnityEngine.Vector2(p.x, p.y);
+        }
+
+        public static implicit operator UnityEngine.Vector3(Point p)
+        {
+            return new UnityEngine.Vector3(p.x, p.y, 0);
+        }
+    }
 }

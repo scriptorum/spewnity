99b8801bc7adc620899f26412dc190f0d1a7c5e3
Eric Lund <github@egotron.com>
2017-12-13 13:28:02 -0500

    NameGizmo shows name of object on object itself

---
+++ Assets/Spewnity/Scripts/Editor/NameGizmo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Spewnity
+{
+	public class NameGizmo : MonoBehaviour
+	{
+		void OnDrawGizmos()
+		{
+			UnityEditor.Handles.Label(transform.position, transform.name);
+		}
+	}
+}
